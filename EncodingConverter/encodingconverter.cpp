49da63f7c947b6fbd0786d8cdb88af6e07d6656f
iBakuman <903926441@qq.com>
2021-07-03 16:57:28 +0800

    最初始的版本

--- EncodingConverter/encodingconverter.cpp
+++ EncodingConverter/encodingconverter.cpp
@@ -1,7 +1,152 @@
 #include "encodingconverter.h"
+#include <QFileDialog>
+#include <QDebug>
+#include <QMessageBox>
+#include <QTextCodec>
 
-EncodingConverter::EncodingConverter(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
+#pragma  execution_character_set("utf-8")
+
+EncodingConverter::EncodingConverter(QWidget* parent)
+	: QMainWindow(parent) {
+	ui.setupUi(this);
+	this->setMaximumSize(this->size());
+	this->setMinimumSize(this->size());
+	QFont font;
+	font.setFamily("Consolas");
+	ui.srcDirText->setFont(font);
+	ui.dstDirText->setFont(font);
+}
+
+void EncodingConverter::saveFile(const QString& content, const QString& aFileName) {
+	if (aFileName.isEmpty())
+		return;
+	QFile aFile(aFileName);
+
+	if (!aFile.open(QIODevice::WriteOnly | QIODevice::Text))
+		return;
+
+	QTextStream out(&aFile);
+	setCodec(out, false);
+	out << content;
+	aFile.close();
+
+}
+
+// 打开目录，返回目录的路径
+QString EncodingConverter::chooseDir() {
+	QFileDialog chooseDirDlg;
+
+	QString srcDirStr = chooseDirDlg.getExistingDirectory(this,
+	                                                      "选择源文件目录",
+	                                                      QDir::currentPath());
+
+	return srcDirStr;
+}
+
+// isSrc为true表示给源文件设置编码，反之给目标文件设置编码
+void EncodingConverter::setCodec(QTextStream& qTextStream, bool isSrc) {
+	Codec tmp = srcCodec;
+	if (!isSrc)
+		tmp = dstCodec;
+	switch (tmp) {
+	case GBK: {
+		qTextStream.setCodec("GBK");
+	}
+	break;
+	case UTF_WITH_BOM: {
+		qTextStream.setCodec("UTF-8");
+		qTextStream.setGenerateByteOrderMark(true); // 带BOM
+	}
+	break;
+	case UTF_8_WITHOUT_BOM: {
+		qTextStream.setCodec("UTF-8");
+		qTextStream.setGenerateByteOrderMark(false);
+	}
+	break;
+	}
+
+}
+
+
+void EncodingConverter::on_actOpenDir_triggered() {
+	qDebug() << "actOpenDir triggered!";
+
+	QString srcDirStr = chooseDir();
+	if (srcDirStr.isEmpty())
+		return;
+
+	QDir srcDir(srcDirStr);
+	if (!srcDir.exists())
+		return;
+
+
+}
+
+void EncodingConverter::on_btnSrcDir_clicked() {
+	// 选择源文件的文件夹
+	ui.srcDirText->clear(); // 清除原来的内容
+	QString srcDirStr = chooseDir();
+	ui.srcDirText->setText(srcDirStr);
+
+}
+
+void EncodingConverter::on_btnDstDir_clicked() {
+	ui.dstDirText->clear(); // 清除原来的内容
+	QString srcDirStr = chooseDir();
+	ui.dstDirText->setText(srcDirStr);
+}
+
+// 开始转换
+void EncodingConverter::on_btnStart_clicked() {
+	QString srcDirStr = ui.srcDirText->text().trimmed();
+	QString dstDirStr = ui.dstDirText->text().trimmed();
+
+
+	if (srcCodec == dstCodec) {
+		QMessageBox::information(this, "提示", "源文件编码和目标编码相同，无需转换!");
+		return;
+	}
+
+	if (srcDirStr.isEmpty() || dstDirStr.isEmpty()) {
+		QMessageBox::warning(this, "警告", "源文件夹或目标文件夹未指定!");
+		return;
+	}
+
+	QDir srcDir(srcDirStr);
+	QFileInfoList fileInfos = srcDir.entryInfoList(QDir::Files); // 遍历普通文件
+	QListIterator<QFileInfo> ite(fileInfos);
+	ui.textEdit->clear();// 清空原来内容
+	while (ite.hasNext()) {
+		const QFileInfo& curFileInfo = ite.next();
+		QString fileName = curFileInfo.fileName();
+		QString filePath = curFileInfo.absoluteFilePath();
+		QFile curFile(filePath);
+		if (!curFile.exists()) // 如果文件不存在
+			continue;
+
+		if (!curFile.open(QIODevice::ReadOnly | QIODevice::Text))
+			continue;
+
+		QTextStream in(&curFile);
+		setCodec(in, true);
+
+		QString content = in.readAll();
+		ui.textEdit->append(fileName);
+		ui.textEdit->append(content);
+		ui.textEdit->append("------------------------");
+
+		saveFile(content, ui.dstDirText->text() + "/" + fileName);
+
+		qDebug() << filePath;
+		qDebug() << fileName;
+		curFile.close();
+	}
+}
+
+void EncodingConverter::on_cboDstEn_currentIndexChanged(int index) {
+	dstCodec = static_cast<Codec>(ui.cboDstEn->currentIndex());
+}
+
+void EncodingConverter::on_cboSrcEn_currentIndexChanged(int index) {
+	srcCodec = static_cast<Codec>(ui.cboSrcEn->currentIndex());
 }
--- EncodingConverter/encodingconverter.h
+++ EncodingConverter/encodingconverter.h
@@ -3,13 +3,35 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_encodingconverter.h"
 
-class EncodingConverter : public QMainWindow
-{
-    Q_OBJECT
+#include <QTextCodec>
+#include <QDir>
+
+class EncodingConverter : public QMainWindow {
+Q_OBJECT
 
 public:
-    EncodingConverter(QWidget *parent = Q_NULLPTR);
+	EncodingConverter(QWidget* parent = Q_NULLPTR);
 
 private:
-    Ui::EncodingConverterClass ui;
+	enum Codec {
+		GBK,
+		UTF_WITH_BOM,
+		UTF_8_WITHOUT_BOM
+	};
+
+	Codec srcCodec = GBK;
+	Codec dstCodec = GBK;
+	Ui::EncodingConverterClass ui;
+	void saveFile(const QString& content, const QString& aFileName);
+	QString chooseDir();
+	void setCodec(QTextStream& qTextStream, bool isSrc);
+	
+
+private slots:
+	void on_actOpenDir_triggered();
+	void on_btnSrcDir_clicked();
+	void on_btnDstDir_clicked();
+	void on_btnStart_clicked();
+	void on_cboDstEn_currentIndexChanged(int index);
+	void on_cboSrcEn_currentIndexChanged(int index);
 };
